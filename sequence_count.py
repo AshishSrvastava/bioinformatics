02deb346d262e1aee594305c2a9283a1d8f2cef4
Ashish Srivastava <fortcollinsashish@gmail.com>
2020-07-09 22:13:48 +0300

    Add files via upload

---
+++ sequence_count.py
@@ -0,0 +1,7 @@
+#defining the sequence counting function
+def print_sequence_length():
+    seq = 'AGTGTCCCTG' #Storing DNA strand as a string in the 'seq' variable
+    print(len(seq)) #prints the length of the sequence stored in 'seq'
+
+if __name__ == "__main__": #telling python to run the code below
+  print_sequence_length() #run our functionpw
